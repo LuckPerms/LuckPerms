dcf207f5e5b874f1e3667a1e0820b9707b0f9f7b
Luck <git@lucko.me>
2016-10-10 22:39:24 +0100

    Fix exception, resolves #20 and #22

--- common/src/main/java/me/lucko/luckperms/api/implementation/internal/PermissionHolderLink.java
+++ common/src/main/java/me/lucko/luckperms/api/implementation/internal/PermissionHolderLink.java
@@ -65,12 +65,12 @@ public class PermissionHolderLink implements PermissionHolder {
 
     @Override
     public Set<Node> getAllNodes() {
-        return Collections.unmodifiableSet(master.getAllNodes(Collections.emptyList(), Contexts.allowAll()));
+        return Collections.unmodifiableSet(master.getAllNodes(null, Contexts.allowAll()));
     }
 
     @Override
     public SortedSet<LocalizedNode> getAllNodes(@NonNull Contexts contexts) {
-        return master.getAllNodes(Collections.emptyList(), contexts);
+        return master.getAllNodes(null, contexts);
     }
 
     @Override
