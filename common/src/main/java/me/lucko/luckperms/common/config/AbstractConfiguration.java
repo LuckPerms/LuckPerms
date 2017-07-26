51898abb717da0477288c0e6596e0721679f9fab
Luck <git@lucko.me>
2017-07-26 10:31:14 +0100

    Don't load contexts file twice

--- common/src/main/java/me/lucko/luckperms/common/config/AbstractConfiguration.java
+++ common/src/main/java/me/lucko/luckperms/common/config/AbstractConfiguration.java
@@ -67,7 +67,6 @@ public abstract class AbstractConfiguration implements LuckPermsConfiguration {
         cache.invalidateAll(toInvalidate);
 
         loadAll();
-        contextsFile.load();
         getPlugin().getApiProvider().getEventFactory().handleConfigReload();
     }
 }
