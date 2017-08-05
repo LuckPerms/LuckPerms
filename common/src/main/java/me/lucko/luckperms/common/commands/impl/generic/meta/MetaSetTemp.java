3951725852a67055317a6c96ab9e48f35ee1b0d0
Luck <git@lucko.me>
2017-08-05 23:46:19 +0200

    Fix 'meta settemp' command using duration as the server context (#421)

--- common/src/main/java/me/lucko/luckperms/common/commands/impl/generic/meta/MetaSetTemp.java
+++ common/src/main/java/me/lucko/luckperms/common/commands/impl/generic/meta/MetaSetTemp.java
@@ -71,7 +71,7 @@ public class MetaSetTemp extends SharedSubCommand {
         String key = args.get(0);
         String value = args.get(1);
         long duration = ArgumentUtils.handleDuration(2, args);
-        MutableContextSet context = ArgumentUtils.handleContext(2, args, plugin);
+        MutableContextSet context = ArgumentUtils.handleContext(3, args, plugin);
         TemporaryModifier modifier = plugin.getConfiguration().get(ConfigKeys.TEMPORARY_ADD_BEHAVIOUR);
 
         if (ArgumentPermissions.checkContext(plugin, sender, permission, context)) {
