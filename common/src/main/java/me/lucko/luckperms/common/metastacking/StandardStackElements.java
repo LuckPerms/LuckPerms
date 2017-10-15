0f4c0573952e0a380685e3da4950afcf9fe9771e
Luck <git@lucko.me>
2017-10-15 15:13:23 +0100

    Add highest_inherited and lowest_inherited meta stack elements

--- common/src/main/java/me/lucko/luckperms/common/metastacking/StandardStackElements.java
+++ common/src/main/java/me/lucko/luckperms/common/metastacking/StandardStackElements.java
@@ -51,6 +51,8 @@ public class StandardStackElements {
     private static final LowestPriority LOWEST_PRIORITY = new LowestPriority();
     private static final HighestPriorityOwn HIGHEST_PRIORITY_OWN = new HighestPriorityOwn();
     private static final LowestPriorityOwn LOWEST_PRIORITY_OWN = new LowestPriorityOwn();
+    private static final HighestPriorityInherited HIGHEST_PRIORITY_INHERITED = new HighestPriorityInherited();
+    private static final LowestPriorityInherited LOWEST_PRIORITY_INHERITED = new LowestPriorityInherited();
 
     public static Optional<MetaStackElement> parseFromString(LuckPermsPlugin plugin, String s) {
         s = s.toLowerCase();
@@ -69,6 +71,14 @@ public class StandardStackElements {
 
         if (s.equals("lowest_own")) {
             return Optional.of(LOWEST_PRIORITY_OWN);
+        }
+
+        if (s.equals("highest_inherited")) {
+            return Optional.of(HIGHEST_PRIORITY_INHERITED);
+        }
+
+        if (s.equals("lowest_inherited")) {
+            return Optional.of(LOWEST_PRIORITY_INHERITED);
         }
 
         if (s.startsWith("highest_on_track_") && s.length() > "highest_on_track_".length()) {
@@ -129,10 +139,6 @@ public class StandardStackElements {
      * @return true if the accumulation should return
      */
     private static boolean checkOwnElement(LocalizedNode node) {
-        if (node.getLocation() == null || node.getLocation().equals("")) {
-            return true;
-        }
-
         try {
             UUID.fromString(node.getLocation());
             return false;
@@ -202,6 +208,23 @@ public class StandardStackElements {
         }
     }
 
+    @ToString
+    private static final class HighestPriorityInherited implements MetaStackElement {
+        @Override
+        public boolean shouldAccumulate(LocalizedNode node, ChatMetaType type, Map.Entry<Integer, String> current) {
+            if (type.shouldIgnore(node)) {
+                return false;
+            }
+
+            if (!checkOwnElement(node)) {
+                return false;
+            }
+
+            Map.Entry<Integer, String> newEntry = type.getEntry(node);
+            return !compareEntriesHighest(current, newEntry);
+        }
+    }
+
     @ToString(of = "trackName")
     @RequiredArgsConstructor
     @EqualsAndHashCode(of = "trackName")
@@ -260,6 +283,23 @@ public class StandardStackElements {
             }
 
             if (checkOwnElement(node)) {
+                return false;
+            }
+
+            Map.Entry<Integer, String> newEntry = type.getEntry(node);
+            return !compareEntriesLowest(current, newEntry);
+        }
+    }
+
+    @ToString
+    private static final class LowestPriorityInherited implements MetaStackElement {
+        @Override
+        public boolean shouldAccumulate(LocalizedNode node, ChatMetaType type, Map.Entry<Integer, String> current) {
+            if (type.shouldIgnore(node)) {
+                return false;
+            }
+
+            if (!checkOwnElement(node)) {
                 return false;
             }
 
