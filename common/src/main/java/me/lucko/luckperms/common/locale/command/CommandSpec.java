8f1f1f6ded194cfc32b2230ccc88dccc9b3bed47
OmegaWeapon_ <Imm0rtalshard@gmail.com>
2019-11-25 02:57:36 +1030

    Add temporary modifier to the argument list for permission/parent settemp (#1817)

--- common/src/main/java/me/lucko/luckperms/common/locale/command/CommandSpec.java
+++ common/src/main/java/me/lucko/luckperms/common/locale/command/CommandSpec.java
@@ -229,6 +229,7 @@ public enum CommandSpec {
                     Argument.create("node", true, "the permission node to set"),
                     Argument.create("true|false", false, "the value of the node"),
                     Argument.create("duration", true, "the duration until the permission node expires"),
+                    Argument.create("temporary modifier", false, "how the temporary permission should be applied"),
                     Argument.create("context...", false, "the contexts to add the permission in")
             )
     ),
@@ -291,6 +292,7 @@ public enum CommandSpec {
             Argument.list(
                     Argument.create("group", true, "the group to inherit from"),
                     Argument.create("duration", true, "the duration of the group membership"),
+                    Argument.create("temporary modifier", false, "how the temporary permission should be applied"),
                     Argument.create("context...", false, "the contexts to inherit the group in")
             )
     ),
