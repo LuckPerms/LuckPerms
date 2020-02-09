7b4162ff54a2fc4fb3fb747a17d6936f8e3edfd5
Luck <git@lucko.me>
2020-02-09 15:24:04 +0000

    Change layout of library storage

--- common/src/main/java/me/lucko/luckperms/common/util/MoreFiles.java
+++ common/src/main/java/me/lucko/luckperms/common/util/MoreFiles.java
@@ -26,6 +26,7 @@
 package me.lucko.luckperms.common.util;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -55,6 +56,24 @@ public final class MoreFiles {
 
         Files.createDirectories(path);
         return path;
+    }
+
+    public static void deleteDirectory(Path path) throws IOException {
+        if (!Files.exists(path) || !Files.isDirectory(path)) {
+            return;
+        }
+
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(path)) {
+            for (Path file : contents) {
+                if (Files.isDirectory(file)) {
+                    deleteDirectory(file);
+                } else {
+                    Files.delete(file);
+                }
+            }
+        }
+
+        Files.delete(path);
     }
 
 }
