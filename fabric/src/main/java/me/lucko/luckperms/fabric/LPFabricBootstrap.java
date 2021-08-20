93683c3a2f55c20bd37c6a7eb28aa088a130f8c8
Luck <git@lucko.me>
2021-08-20 18:46:39 +0100

    Update Fabric to 1.17, fix version placeholder filtering

--- fabric/src/main/java/me/lucko/luckperms/fabric/LPFabricBootstrap.java
+++ fabric/src/main/java/me/lucko/luckperms/fabric/LPFabricBootstrap.java
@@ -230,13 +230,13 @@ public final class LPFabricBootstrap implements LuckPermsBootstrap, DedicatedSer
 
     @Override
     public Optional<UUID> lookupUniqueId(String username) {
-        return getServer().map(MinecraftServer::getUserCache).map(c -> c.findByName(username)).map(GameProfile::getId);
+        return getServer().map(MinecraftServer::getUserCache).flatMap(c -> c.findByName(username)).map(GameProfile::getId);
 
     }
 
     @Override
     public Optional<String> lookupUsername(UUID uniqueId) {
-        return getServer().map(MinecraftServer::getUserCache).map(c -> c.getByUuid(uniqueId)).map(GameProfile::getName);
+        return getServer().map(MinecraftServer::getUserCache).flatMap(c -> c.getByUuid(uniqueId)).map(GameProfile::getName);
     }
 
     @Override
