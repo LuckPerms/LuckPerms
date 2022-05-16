48ffde76755a2a21a8a93676761a5f1b21a5a430
Alex Thomson <LXGaming@users.noreply.github.com>
2022-05-16 23:08:30 +1200

    Support GenericEvent and IModBusEvent events

--- forge/src/main/java/me/lucko/luckperms/forge/util/EventBusUtil.java
+++ forge/src/main/java/me/lucko/luckperms/forge/util/EventBusUtil.java
@@ -27,7 +27,12 @@ package me.lucko.luckperms.forge.util;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.GenericEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.IGenericEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.IModBusEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import java.lang.invoke.CallSite;
 import java.lang.invoke.LambdaMetafactory;
@@ -36,14 +41,15 @@ import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.function.Consumer;
 
 public class EventBusUtil {
 
     private static final MethodHandles.Lookup LOOKUP = MethodHandles.lookup();
 
-    @SuppressWarnings("unchecked")
-    public static <T extends Event> void register(Object target) {
+    public static void register(Object target) {
         for (Method method : target.getClass().getMethods()) {
             if (Modifier.isStatic(method.getModifiers())) {
                 continue;
@@ -54,7 +60,7 @@ public class EventBusUtil {
                 continue;
             }
 
-            Class<?>[] parameterTypes = method.getParameterTypes();
+            Type[] parameterTypes = method.getGenericParameterTypes();
             if (parameterTypes.length != 1) {
                 throw new IllegalArgumentException(""
                         + "Method " + method + " has @SubscribeEvent annotation. "
@@ -63,7 +69,49 @@ public class EventBusUtil {
                 );
             }
 
-            Class<?> eventType = parameterTypes[0];
+            Type parameterType = parameterTypes[0];
+            Class<?> eventType;
+            Class<?> genericType;
+            if (parameterType instanceof Class) {
+                eventType = (Class<?>) parameterType;
+                genericType = null;
+            } else if (parameterType instanceof ParameterizedType) {
+                ParameterizedType parameterizedType = (ParameterizedType) parameterType;
+
+                Type rawType = parameterizedType.getRawType();
+                if (rawType instanceof Class) {
+                    eventType = (Class<?>) rawType;
+                } else {
+                    throw new UnsupportedOperationException("Raw Type " + rawType.getClass() + " is not supported");
+                }
+
+                Type[] typeArguments = parameterizedType.getActualTypeArguments();
+                if (typeArguments.length != 1) {
+                    throw new IllegalArgumentException(""
+                            + "Method " + method + " has @SubscribeEvent annotation. "
+                            + "It has a " + eventType + " argument, "
+                            + "but generic events require a single type argument only."
+                    );
+                }
+
+                Type typeArgument = typeArguments[0];
+                if (typeArgument instanceof Class<?>) {
+                    genericType = (Class<?>) typeArgument;
+                } else {
+                    throw new UnsupportedOperationException("Type Argument " + typeArgument.getClass() + " is not supported");
+                }
+            } else {
+                throw new UnsupportedOperationException("Parameter Type " + parameterType.getClass() + " is not supported");
+            }
+
+            if (GenericEvent.class.isAssignableFrom(eventType) && genericType == null) {
+                throw new IllegalArgumentException(""
+                        + "Method " + method + " has @SubscribeEvent annotation, "
+                        + "but the generic argument type cannot be determined for "
+                        + "for the GenericEvent subtype: " + eventType
+                );
+            }
+
             if (!Event.class.isAssignableFrom(eventType)) {
                 throw new IllegalArgumentException(""
                         + "Method " + method + " has @SubscribeEvent annotation, "
@@ -71,7 +119,7 @@ public class EventBusUtil {
                 );
             }
 
-            Consumer<T> consumer;
+            Consumer<?> consumer;
             try {
                 MethodHandle methodHandle = LOOKUP.unreflect(method);
                 CallSite callSite = LambdaMetafactory.metafactory(
@@ -83,13 +131,34 @@ public class EventBusUtil {
                         MethodType.methodType(void.class, eventType)
                 );
 
-                consumer = (Consumer<T>) callSite.getTarget().bindTo(target).invokeExact();
+                consumer = (Consumer<?>) callSite.getTarget().bindTo(target).invokeExact();
             } catch (Throwable t) {
                 throw new RuntimeException("Error whilst registering " + method, t);
             }
 
-            MinecraftForge.EVENT_BUS.addListener(subscribeEvent.priority(), subscribeEvent.receiveCanceled(), (Class<T>) eventType, consumer);
+            IEventBus eventBus;
+            if (IModBusEvent.class.isAssignableFrom(eventType)) {
+                eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+            } else {
+                eventBus = MinecraftForge.EVENT_BUS;
+            }
+
+            if (IGenericEvent.class.isAssignableFrom(eventType)) {
+                addGenericListener(eventBus, genericType, subscribeEvent, eventType, consumer);
+            } else {
+                addListener(eventBus, subscribeEvent, eventType, consumer);
+            }
         }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends GenericEvent<? extends F>, F> void addGenericListener(IEventBus eventBus, Class<?> genericClassFilter, SubscribeEvent subscribeEvent, Class<?> eventType, Consumer<?> consumer) {
+        eventBus.addGenericListener((Class<F>) genericClassFilter, subscribeEvent.priority(), subscribeEvent.receiveCanceled(), (Class<T>) eventType, (Consumer<T>) consumer);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T extends Event> void addListener(IEventBus eventBus, SubscribeEvent subscribeEvent, Class<?> eventType, Consumer<?> consumer) {
+        eventBus.addListener(subscribeEvent.priority(), subscribeEvent.receiveCanceled(), (Class<T>) eventType, (Consumer<T>) consumer);
     }
 
 }
