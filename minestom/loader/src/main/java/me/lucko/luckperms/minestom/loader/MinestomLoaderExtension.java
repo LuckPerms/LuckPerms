28b574151dd03577668dc292d6c748a0b507e369
dev-hydrogen <96733109+dev-hydrogen@users.noreply.github.com>
2022-07-02 19:48:33 +0300

    use extension improvement branch

--- minestom/loader/src/main/java/me/lucko/luckperms/minestom/loader/MinestomLoaderExtension.java
+++ minestom/loader/src/main/java/me/lucko/luckperms/minestom/loader/MinestomLoaderExtension.java
@@ -40,14 +40,22 @@ public class MinestomLoaderExtension extends Extension {
         this.plugin = loader.instantiatePlugin(BOOTSTRAP_CLASS, Extension.class, this);
     }
 
-    @Override
     public void preInitialize() {
         this.plugin.onLoad();
+        logger().debug("Void Preinit called");
     }
 
+    /*public LoadStatus preInitialize() {
+        this.plugin.onLoad();
+        logger().debug("Preinit called");
+        return LoadStatus.SUCCESS;
+    }*/
+
     @Override
-    public void initialize() {
+    public LoadStatus initialize() {
+        this.plugin.onLoad();
         this.plugin.onEnable();
+        return LoadStatus.SUCCESS;
     }
 
     @Override
--- minestom/src/main/java/me/lucko/luckperms/minestom/LPMinestomBootstrap.java
+++ minestom/src/main/java/me/lucko/luckperms/minestom/LPMinestomBootstrap.java
@@ -128,7 +128,7 @@ public class LPMinestomBootstrap implements LuckPermsBootstrap, LoaderBootstrap,
 
     @Override
     public String getVersion() {
-        return loader.getOrigin().getVersion();
+        return loader.descriptor().version();
     }
 
     @Override
